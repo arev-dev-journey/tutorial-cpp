7ebf4792c2757bb35c848587ca1a005e9e9d5dc4
Alex Reveles <areveles@student.umgc.edu>
2025-02-10 15:18:57 -0800

    Added new learning files

--- pointers.cpp
+++ pointers.cpp
@@ -6,21 +6,5 @@ using namespace std;
 
 int main() {
 
-  int *p;
-  p=new int[5];
-
-  p[0]=10;
-  p[1]=15;
-  p[2]=14;
-  p[3]=21;
-  p[4]=31;
-
-  for(int i=0;i<5;i++)
-  {
-    cout<<p[i]<<endl;
-  }
-
-  delete [] p;
   return 0;
-  
 }
